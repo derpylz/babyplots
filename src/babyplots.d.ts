89a7d0e1941a236980ae457ed34b3a854bc05943
derpylz <nils.trost@hotmail.de>
2022-07-11 09:04:35 +0200

    Remove some superfluous imports.

--- src/babyplots.d.ts
+++ src/babyplots.d.ts
@@ -170,7 +170,7 @@ export declare class Plots {
     constructor(canvasElement: string, options?: {});
     private _updateCameraUpVector;
     fromJSON(plotData: {}): void;
-    createButtons(whichBtns?: string[]): void;
+    createButtons(whichBtns?: string[]): Plots;
     private _prepDownloadObj;
     private _downloadJson;
     private _createPublishForm;
@@ -178,10 +178,10 @@ export declare class Plots {
     private _tryPublish;
     private _cancelPublish;
     private _resetAnimation;
-    pauseAnimation(): void;
-    playAnimation(): void;
-    toggleTurntable(): void;
-    setAnimationFrame(): void;
+    pauseAnimation(): Plots;
+    playAnimation(): Plots;
+    toggleTurntable(): Plots;
+    setAnimationFrame(): Plots;
     private _toggleLoopAnimation;
     private _startRecording;
     private _prepRender;
@@ -189,7 +189,7 @@ export declare class Plots {
     private _cameraFitPlot;
     addImgStack(values: number[], indices: number[], attributes: {
         dim: number[];
-    }, options: {}): this;
+    }, options: {}): Plots;
     addPlot(coordinates: number[][], plotType: string, colorBy: string, colorVar: string[] | number[], options?: {}): Plots;
     addMeshObject(meshString: string, options: {}): Plots;
     addMeshStream(rootUrl: string, filePrefix: string, fileSuffix: string, fileIteratorStart: number, fileIteratorEnd: number, frameDelay: number, options: {}): Plots;
@@ -199,8 +199,8 @@ export declare class Plots {
     private _createShapeLegend;
     doRender(): Plots;
     resize(width?: number, height?: number): Plots;
-    thumbnail(size: number, saveCallback: (data: string) => void): void;
+    thumbnail(size: number, saveCallback: (data: string) => void): Plots;
     dispose(): void;
     removePlot(index: number): Plots;
-    addLabels(labelList: [[number, number, number, string, string?, number?]]): void;
+    addLabels(labelList: [[number, number, number, string, string?, number?]]): Plots;
 }
